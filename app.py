2a1c06076847c4424ebb3858e2316c7473290e53
Aleen Dhar <86429480+AleenDhar@users.noreply.github.com>
2024-03-20 21:28:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,16 +39,6 @@ def process_input(urls, question):
     )
     return after_rag_chain.invoke(question)
 
-import pyttsx3
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-# print(voices[1].id)
-engine.setProperty('voice', voices[0].id)
-
-
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
 
 
 # Define Gradio interface
